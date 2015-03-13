c2e43978e43d5d5da2839046e779ef060c4af604
Christian Soeller <c.soeller@gmail.com>
2015-03-13 16:26:52 +0000

    mapdirpath added

--- dh5view.sh
+++ dh5view.sh
@@ -2,6 +2,8 @@
 PATH="/Library/Frameworks/Python.framework/Versions/Current/bin:${PATH}"
 # echo $PATH
 # echo $PYTHONPATH
+export PYMEZYLAMAPDIR=/Users/csoe002/Documents/data/LM/Zyla/maps/blemishcurrent
 PYTHONPATH=$HOME/Documents/src/PYMEcurrent/:$HOME/Documents/src/PYME-nf/ dh5view.py "$*"
+    
 #echo 'Arguments'
 #echo $*
--- visgui.sh
+++ visgui.sh
@@ -3,6 +3,7 @@ PATH="/Library/Frameworks/Python.framework/Versions/Current/bin:${PATH}"
 # echo $PATH
 # echo $PYTHONPATH
 # force file concatenation
+export PYMEZYLAMAPDIR=/Users/csoe002/Documents/data/LM/Zyla/maps/blemishcurrent
 PYTHONPATH=$HOME/Documents/src/PYMEcurrent/:$HOME/Documents/src/PYME-nf/ VisGUI.py "$*"
 #echo 'Arguments'
 #echo $*
