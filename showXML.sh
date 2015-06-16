c6282b070b2e18028f31497a0f1c9cd31d5bf374
Christian Soeller <c.soeller@gmail.com>
2015-06-16 09:06:46 +0100

    fix up and improve showxml

--- showXML.sh
+++ showXML.sh
@@ -4,10 +4,22 @@ PATH="${HOME}/anaconda/bin:${PATH}"
 # echo $PATH
 # echo $PYTHONPATH
 FNAME=$1
-export PYTHONPATH=/Users/csoelle/Documents/src/PYME-nf/
-export python - <<EOF
+# export PYTHONPATH=/Users/csoelle/Documents/src/PYME-nf/
+python - <<EOF
+import time
 from PYME.Acquire import MetaDataHandler
-print MetaDataHandler.XMLMDHandler("$FNAME")
+mdh =  MetaDataHandler.XMLMDHandler("$FNAME")
+print mdh
+try:
+    print 'StartTime in date format: %s' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mdh['StartTime']))
+except:
+    pass
+
+try:
+    print 'Source.StartTime in date format: %s' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mdh['Source.StartTime']))
+except:
+    pass
+
 EOF
 
 #echo 'Arguments'
