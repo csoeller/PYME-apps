1362787e2f4b48b20f6ed9f80006aa8bed97262b
Christian Soeller <c.soeller@gmail.com>
2017-10-23 10:19:11 +0100

    change PYTHONPATH

--- visgui.sh
+++ visgui.sh
@@ -1,8 +1,7 @@
 #!/opt/local/bin/bash
 PATH="${HOME}/anaconda/bin:${PATH}"
 
-export PYMEZYLAMAPDIR=/Users/csoe002/Documents/data/LM/Zyla/maps/blemishcurrent
-export PYTHONPATH=$HOME/Documents/src/PYME-nf/
+export PYTHONPATH=$HOME/Documents/src/PYME-extra
 echo "Opening $1..."
 
 (nohup VisGUI.py "$*" >"/tmp/visgui-$BASHPID.tmp" 2>&1 &)
